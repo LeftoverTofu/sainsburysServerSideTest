8658de1b72aa1ac4f57989ccb847f35931ee19bc
Soi-Phun Chuong <leftovertofugames@gmail.com>
2020-09-17 17:55:50 +0100

    Created SainsburyScraper with test class with initial scrapForAllProducts method taking in url as string

---
+++ src/main/java/sainsburys/scrape/app/scrape/SainsburyScraper.java
@@ -0,0 +1,12 @@
+package sainsburys.scrape.app.scrape;
+
+import org.json.JSONArray;
+
+public class SainsburyScraper {
+
+	public JSONArray scapeForAllProducts(String url) {
+		return null;
+	}
+	
+	
+}
---
+++ src/test/java/sainsburys/scrape/app/scrape/SainsburyScraperTest.java
@@ -0,0 +1,14 @@
+package sainsburys.scrape.app.scrape;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SainsburyScraperTest {
+
+	@Test
+	public void testScapeForAllProducts() {
+		fail("Not yet implemented");
+	}
+
+}
